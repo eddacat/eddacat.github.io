e5d9c497cc32e839e42737a6de1318de2db46fc6
eddacat <edda.k.m@gmail.com>
2018-10-17 10:51:27 +0000

    up

--- P0-HalloHeimur/sketch.js
+++ P0-HalloHeimur/sketch.js
@@ -1,1 +1,12 @@
 // Hér kemur kóðinn þinn:
+function setup(){
+    createCanvas(300,300);
+    background(255,90,67);
+    fill(150);
+    ellipse(100,100,50,50);
+    fill(0);
+    textSize(20);
+    text("-~*¨°Halló heimur°¨*~-",50,40);
+    rect(200,200,50,50,10);
+    ellipse(100,200,50,50);
+}
