c10b2ccc7a94cfb9fa24d176cb5aee306bdc8ea5
eddacat <edda.k.m@gmail.com>
2018-10-24 10:05:16 +0000

    blushcol

--- P2-Andlit/sketch.js
+++ P2-Andlit/sketch.js
@@ -27,7 +27,7 @@ function draw(){
   noStroke();
   rect(150, 175, mouthLength, 5, 1, 1, 10, 10);
   rectMode(CENTER);
-  fill(84, 117, 119);
+  fill(255, 122, 101);
   noStroke();
   rect(175, 165, 20, 10, 5);
   rect(125, 165, 20, 10, 5);
