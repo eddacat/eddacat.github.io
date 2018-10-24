2545c095d6c9fedcdaa875d3794319de7433d580
eddacat <edda.k.m@gmail.com>
2018-10-24 10:02:07 +0000

    blu

--- P2-Andlit/sketch.js
+++ P2-Andlit/sketch.js
@@ -25,6 +25,11 @@ function draw(){
   fill(255, 144, 133);
   noStroke();
   rect(150, 175, mouthLength, 5, 1, 1, 10, 10);
+  rectMode(CENTER);
+  fill(84, 117, 119);
+  noStroke();
+  rect(175, 165, 20, 10, 5);
+  rect(125, 165, 20, 10, 5);
   //.....kóði sem staðsetur augun m.v. x, y, faceWidth og faceHeight ætti að koma hér.
 
 }
