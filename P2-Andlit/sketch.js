4d5014470892f8cfd54214bf278f61a5a2f2ce72
eddacat <edda.k.m@gmail.com>
2018-10-24 08:55:53 +0000

    augu

--- P2-Andlit/sketch.js
+++ P2-Andlit/sketch.js
@@ -1,26 +1,26 @@
 // Hér kemur kóðinn þinn:
+var eyeSize = 20;
+var faceWidth = 100;
+var faceHeight = 150;
+var x = 150;
+var y = 150;
+
 function setup(){
-    createCanvas(300,300);
-    background(220,41,16);
-    rect(125,0,5,300,);
-    rect(0,160,300,5,);
-    rect(125,280,150,5,);
-    rect(270,165,5,150,);
-    rect(270,215,50,5,);
+  createCanvas (300,300);
+}
 
-    fill(250);
-    rect(130,0,170,160,);
-    fill(250);
-    rect(0,165,125,145,);
-    fill(250);
-    rect(130,165,140,115,);
-    fill(250);
-    rect(275,220,25,80,);
+function draw(){
+  background(180);
+  rect(x, y, faceWidth, faceHeight, 30);
+  rect(200, 150, eyeSize, eyeSize, 10, 10, 2, 2)
+  rect(150, 200, eyeSize, eyeSize, 10, 10, 2, 2)
+  //.....kóði sem staðsetur augun m.v. x, y, faceWidth og faceHeight ætti að koma hér.
 
-    fill(255, 204, 0);
-    rect(130,285,75,20,);
-    fill(0);
-    rect(205,285,65,20,);
-    fill(16,67,220);
-    rect(275,165,25,50,);
+}
+
+function mousePressed(){
+  //Í hvert sinn sem músinni er smellt fá breyturnar ný gildi.
+  faceWidth  = random(75,  150);
+  faceHeight = random(100, 200);
+  eyeSize    = random(10,  30);
 }
