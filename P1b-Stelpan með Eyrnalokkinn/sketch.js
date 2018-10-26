d23b1a3ae13f78ffb990fb496b08169c4fa070db
eddacat <edda.k.m@gmail.com>
2018-10-26 13:19:50 +0000

    litir

--- P1b-Stelpan með Eyrnalokkinn/sketch.js
+++ P1b-Stelpan með Eyrnalokkinn/sketch.js
@@ -1,8 +1,9 @@
 // Hér kemur kóðinn þinn:
 function setup(){
     createCanvas(300,300);
-    background(220,41,16);
+    background(28, 25, 25);
 
+    fill(157, 115, 89)
     quad(100,300, 150,300, 130,150, 100,175);
 
 }
