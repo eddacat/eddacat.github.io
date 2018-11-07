c497d48b094831fc333adbc6ca3cef6f3026df18
eddacat <edda.k.m@gmail.com>
2018-11-07 09:28:48 +0000

    s

--- P5-Sprætur/sketch.js
+++ P5-Sprætur/sketch.js
@@ -2,8 +2,8 @@ var bakgrunnur;
 
 function preload() {
   bakgrunnur = loadImage('assets/bg.png_large'); //https://twitter.com/16pxl/status/1059615735964688385
+   }
 function setup() {
   createCanvas(700,700);
   background(bakgrunnur);
-   }
 }
